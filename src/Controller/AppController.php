a9d8cd1c89669fd6ed6f21443249493d7e8dd9d0
valh-runner <valh.trust@free.fr>
2021-06-03 19:25:51 +0200

    deletion of unused variables

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
@@ -14,7 +12,7 @@ class AppController extends AbstractController
     /**
      * @Route("/api/products", name="api_product_index", methods={"GET"})
      */
-    public function index(ProductRepository $productRepository, SerializerInterface $serializer): Response
+    public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
         $response = $this->json($products, 200, [], ['groups' => 'product:index']);
@@ -24,7 +22,7 @@ class AppController extends AbstractController
     /**
      * @Route("/api/products/{id}", name="api_product_detail", methods={"GET"})
      */
-    public function detail(int $id, ProductRepository $productRepository, SerializerInterface $serializer): Response
+    public function detail(int $id, ProductRepository $productRepository): Response
     {
         $product = $productRepository->findOneBy(['id' => $id]);
         $response = $this->json($product, 200, [], ['groups' => 'product:read']);
