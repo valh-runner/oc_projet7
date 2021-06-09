bb0a8f6868baaeb45e11d54a96c28d5631428f12
valh-runner <valh.trust@free.fr>
2021-06-09 15:00:06 +0200

    setting up authentication and authorization

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -14,16 +14,18 @@ class AppController extends AbstractController
      */
     public function index(ProductRepository $productRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER'); //restrict access to users and admin
         $products = $productRepository->findAll();
         $response = $this->json($products, 200, [], ['groups' => 'product:index']);
         return $response;
     }
 
     /**
-     * @Route("/api/products/{productId}", name="api_product_detail", methods={"GET"})
+     * @Route("/api/products/{productId<\d+>}", name="api_product_detail", methods={"GET"})
      */
     public function detail(int $productId, ProductRepository $productRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER'); //restrict access to users and admin
         $product = $productRepository->findOneBy(['id' => $productId]);
         $response = $this->json($product, 200, [], ['groups' => 'product:read']);
         return $response;
