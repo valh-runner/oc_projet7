05b3468b4ae13e7625bfa0c30957b44e966e72f3
valh-runner <valh.trust@free.fr>
2021-06-12 15:56:06 +0200

    JWT system errors customization

---
+++ src/EventListener/LexikJWTErrorsListener.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTInvalidEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTNotFoundEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+
+class LexikJWTErrorsListener
+{
+
+    /**
+     * @param AuthenticationFailureEvent $event
+     */
+    public function onAuthenticationFailureResponse(AuthenticationFailureEvent $event)
+    {
+        $response = new JWTAuthenticationFailureResponse('Informations de connexion non valides', JsonResponse::HTTP_UNAUTHORIZED);
+        $event->setResponse($response);
+    }
+
+    /**
+     * @param JWTInvalidEvent $event
+     */
+    public function onJWTInvalid(JWTInvalidEvent $event)
+    {
+        $response = new JWTAuthenticationFailureResponse('Jeton d\'identification JWT non valide', JsonResponse::HTTP_UNAUTHORIZED);
+        $event->setResponse($response);
+    }
+
+    /**
+     * @param JWTNotFoundEvent $event
+     */
+    public function onJWTNotFound(JWTNotFoundEvent $event)
+    {
+        $data = [
+            'status'  => '401 Unauthorized',
+            'message' => 'Jeton d\'identification JWT manquant',
+        ];
+        $response = new JsonResponse($data, JsonResponse::HTTP_UNAUTHORIZED);
+        $event->setResponse($response);
+    }
+
+    /**
+     * @param JWTExpiredEvent $event
+     */
+    public function onJWTExpired(JWTExpiredEvent $event)
+    {
+        /** @var JWTAuthenticationFailureResponse */
+        $response = $event->getResponse();
+        $response->setMessage('Jeton d\'identification JWT expiré');
+    }
+}
