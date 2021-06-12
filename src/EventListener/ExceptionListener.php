cad9686ffef7541f156c45e09ac1372b1fdd7e35
valh-runner <valh.trust@free.fr>
2021-06-12 19:07:51 +0200

    best practices code corrections

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -17,48 +17,51 @@ class ExceptionListener
     {
         $exception = $event->getThrowable(); // get the exception from the event
         $response = new JsonResponse();
-        $message = [];
+        $data = [];
 
         // if type of exception is HttpExceptionInterface it holds status code and header details
         if ($exception instanceof HttpExceptionInterface) {
             $statusCode = $exception->getStatusCode();
-            $message['status'] = $statusCode;
+            $data['status'] = $statusCode;
             $response->setStatusCode($statusCode);
             $response->headers->replace($exception->getHeaders());
 
             switch ($statusCode) {
                 case '400':
-                    $message['message'] = 'La requête ne peut être traitée correctement';
+                    $data['message'] = 'La requête ne peut être traitée correctement';
                     break;
                 case '401':
-                    $message['message'] = 'L\'authentification a échoué';
+                    $data['message'] = 'L\'authentification a échoué';
                     break;
                 case '403':
-                    $message['message'] = 'L\'accès à cette ressource n\'est pas autorisé';
+                    $data['message'] = 'L\'accès à cette ressource n\'est pas autorisé';
                     break;
                 case '404':
-                    $message['message'] = 'La ressource n\'existe pas';
+                    $data['message'] = 'La ressource n\'existe pas';
                     break;
                 case '405':
-                    $message['message'] = 'La méthode HTTP utilisée n\'est pas traitable par l\'API';
+                    $data['message'] = 'La méthode HTTP utilisée n\'est pas traitable par l\'API';
                     break;
                 case '406':
-                    $message['message'] = 'Le serveur n\'est pas en mesure de répondre aux attentes des entêtes';
+                    $data['message'] = 'Le serveur n\'est pas en mesure de répondre aux attentes des entêtes';
                     break;
                 case '500':
-                    $message['message'] = 'Le serveur a rencontré un problème';
+                    $data['message'] = 'Le serveur a rencontré un problème';
                     break;
                 default:
-                    $message['message'] = $exception->getMessage();
+                    $data['message'] = $exception->getMessage();
             }
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-            $message['error code'] = $exception->getCode();
-            $message['message'] = $exception->getMessage();
-        }
-        $response->setData($message);
 
-        // sends the modified response object to the event
-        $event->setResponse($response);
+            $response->setData($data);
+            $event->setResponse($response); // sends the modified response object to the event
+            return;
+        }
+
+        $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        $data['error code'] = $exception->getCode();
+        $data['message'] = $exception->getMessage();
+
+        $response->setData($data);
+        $event->setResponse($response); // sends the modified response object to the event
     }
 }
